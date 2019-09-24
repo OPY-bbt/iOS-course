ea3bb12679d2cbeafba6e36343f2f5536c850ef2
zhangyan <zhyabs@gmail.com>
2019-09-24 23:38:56 +0800

    feat: day2 UITableView and reuse cell and delegate and datasource

--- Day2/Day2/ViewController.m
+++ Day2/Day2/ViewController.m
@@ -8,35 +8,48 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
 
 @end
 
 @implementation ViewController
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    // 去系统复用回收池里取相同identifier的cell,取不到再新建
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"id"];
+    if(!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat: @"TITLE - %@", @(indexPath.row)];
+    cell.detailTextLabel.text = @"SUBTITLE";
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 100;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"%@", @(indexPath.row));
+    UIViewController *viewController = [[UIViewController alloc] init];
+    viewController.title = [NSString stringWithFormat: @"%@", @(indexPath.row)];
+    [self.navigationController pushViewController:viewController animated:YES];
+    
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self.view addSubview:({
-        UIView *view = [[UIView alloc] init];
-        view.backgroundColor = [UIColor blackColor];
-        view.frame = CGRectMake(100, 100, 100, 100);
-        UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(pushController)];
-        [view addGestureRecognizer:tapGesture];
-        view;
-    })];
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
+    tableView.dataSource = self;
+    tableView.delegate = self;
+    [self.view addSubview:tableView];
 }
-
--(void) pushController{
-    UIViewController *viewController = [[UIViewController alloc] init];
-    viewController.view.backgroundColor = [UIColor whiteColor];
-    viewController.navigationItem.title = @"content";
-    
-    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"rightBtn" style:UIBarButtonItemStylePlain target:self action:nil];
-    
-    [self.navigationController pushViewController:viewController animated:true];
-}
-
 
 @end
