aef0397decbef799e7ed387e23f65c25930b0740
zhangyan <zhyabs@gmail.com>
2019-09-28 19:45:03 +0800

    feat: UILabel, CustomTableViewCell

---
+++ Day5/Day2/HomeTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  HomeTableViewCell.h
+//  Day2
+//
+//  Created by 张岩 on 2019/9/28.
+//  Copyright © 2019 zyGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeTableViewCell : UITableViewCell
+
+-(void) layoutTableViewCell;
+
+@end
+
+NS_ASSUME_NONNULL_END
